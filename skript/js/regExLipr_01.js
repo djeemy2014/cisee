56fb26de10904383932e5c288ef5e0231d94e617
Demidyuk Dmitiy <djeemy2014@gmail.com>
2023-05-09 16:54:43 +0300

    url.json

--- skript/js/regExLipr_01.js
+++ skript/js/regExLipr_01.js
@@ -1,19 +1,18 @@
 import XLSX from 'xlsx';
 import fs from 'fs'
 
-const input_xlsx = "O:\\Градостроительство\\2022\\СТП Липецкой области\\03 ИСХОДН_ИНФ\\полезные ископаемые\\Плоская таблица (1).xlsx";
-const output_xlsx = "C:\\Users\\ddemidyuk\\Downloads\\ALL20230329.xlsx"; //Чернушенский Соликамский ALL20230313
+const input_xlsx = "C:\\Users\\Администратор\\Desktop\\полезные ископаемые\\Плоская таблица (2).xlsx";
+const output_path = "C:\\Users\\Администратор\\Desktop\\"; //Чернушенский Соликамский ALL20230313
 const regexpCoordSys=/Система координат - \S{0,50}/g;
 const regexpType=/Тип пространственного объекта - \S{0,50}/g;
 //const regexpType=new RegExp('Тип пространственного объекта - \S{0,50}','g');
 //const regexpTable=/№ точки .{0,100}(.{0,100}(\r\n)){3,}/g;
 const regexpTable=/(\d.{0,200}((Е|E)\s{0,}\n)){3,}/g;
-const nameOutputFile='testing_JS_3'
+const nameOutputFile='testing_JS_2'
 
 const geojson={
     "type": "FeatureCollection",
     "name": nameOutputFile,
-    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
     "features": []
 }
 
@@ -31,7 +30,8 @@ function calcToDec(str){
     //console.log(str)
     let grad=parseFloat(str.match(/^\d{1,3}/)[0])
     let min=parseFloat(str.match(/\d{1,2}(?=')/)[0])
-    let sec=parseFloat(str.match(/(\d{1,2}\.\d{1,})|(\d{1,2})/)[0])
+    let sec=parseFloat(str.match(/(\d{1,2}\.\d{1,})|(\d{1,2})(?=")/)[0])
+    //console.log(grad,min,sec)
     //console.log(grad+min/60+sec/3600)
     return grad+min/60+sec/3600
 }
@@ -48,23 +48,33 @@ function createPoligon(arr){
         endList[index][0]= calcToDec(elem[2])
         //console.log(endList[index])
     })
+   
+    if (endList[0][0]==endList[endList.length-1][0]
+        &&
+        endList[0][1]==endList[endList.length-1][1]){
+        console.log('JR')
+    }else{
+        endList.push(endList[0])
+        //console.log (arr)
+    }
     //console.log(endList)
-    return [endList]
+    return [endList.reverse()]
 }
 //createPoligon(arrrrrr)
 //выборка по условию
-//console.log(list[76])
+console.log(list.length)
 list.forEach((res, index) => {
     
     let selectColon=res['Географические координаты угловых точек участка недр, верхняя и нижняя границы участка недр']
     //
-    //console.log(index)
+    console.log(index)
     //console.log(selectColon)
-    if (regexpCoordSys.test(selectColon)){
+    if (regexpCoordSys.test(selectColon)&&regexpTable.test(selectColon)){
+        console.log(selectColon)
         let arrCoordSys=selectColon.match(regexpCoordSys)
         let arrType=selectColon.match(regexpType)
         let arrTable=selectColon.match(regexpTable)
-        //console.dir(arrTable)
+        console.dir(arrTable)
         let mPlogon=[]
         arrTable.forEach((ev, index)=>{
             let list =ev.split('\r\n')
@@ -80,11 +90,12 @@ list.forEach((res, index) => {
         res.typeCoordSys=arrCoordSys;
         geojson.features[index]={
             "type": "Feature",
-            "properties":res,
             "geometry":{ 
-                "type": "Collection", 
-                "coordinates":mPlogon
-            }
+                type: "MultiPolygon", 
+                coordinates:mPlogon
+            },
+            "properties":res
+           
         }
         //console.log(index, mPlogon)
         //inputDatd.push(res);
@@ -96,6 +107,6 @@ list.forEach((res, index) => {
 
 });
 console.log(JSON.stringify(geojson))
-let data=JSON.stringify(geojson)
-let url='C:\\Users\\ddemidyuk\\Desktop\\'+nameOutputFile+'.json'
+let data=JSON.stringify(geojson, null, 2)
+let url=output_path+nameOutputFile+'.geojson'
 fs.writeFile(url,data, 'utf8', err=>{console.log(err)})
