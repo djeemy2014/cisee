7e7d87ba837d4655cbbf8d0629e330e45559933b
Dmitiy Demidyuk work <djeemy2014@gmail.com>
2023-05-26 21:18:33 +0300

    3

--- skript/js/rosgeologiy/read_skript.js
+++ skript/js/rosgeologiy/read_skript.js
@@ -8,13 +8,17 @@ import gdal from 'gdal-async';
 let input_data = JSON.parse( fs.readFileSync('./dagestan.json')).result.data
 
 //регулярки
-const regexpCoordSys=/(?<=(Система координат - ))\S{0,50}/;
+const regexpCoordSys=/(?<=(Система координат - ))\S{0,50}/g;
 const regexpType=/(?<=(Тип пространственного объекта - ))\S{0,50}/g;
+//const regexpString=/\d{1,3}.{30,50}([E]|[Е])$/
+const regexpString=/(?<=\n)\d{1,3}.{30,50}(([E](\s){0,8})|([Е](\s){0,8}))/g
+const regexpTable=/((?<=\n)\d{1,3}(\s).{8,12}°.{20,40}(([E](\s){0,10})|([Е](\s){0,10}))){1,100}/g;
 
 let output_data=[]
 let output_data_point=[]
 let output_data_line=[]
 let output_data_poligon=[]
+let output_data_collection=[]
 let output_data_NaN=[]
 let output_data_tible=[]
 
@@ -29,29 +33,61 @@ function calcToDec(str){
     //console.log(grad+min/60+sec/3600)
     return grad+min/60+sec/3600
 }
-//функция создания точки
-function createPoligon(arr){
+//функция создания точек
+function createPoint(arr){
     let endList=[]
     arr.forEach((ev, index)=>{
         endList[index]=[]
         let elem=ev.split(/\s{1,}/)
         //console.log(elem)
-        //endList[index][0]= parseFloat(elem[0])
-        endList[index][1]= calcToDec(elem[1])
-        endList[index][0]= calcToDec(elem[2])
+        endList[index][0]= parseFloat(elem[0])
+        endList[index][2]= calcToDec(elem[1])
+        endList[index][1]= calcToDec(elem[2])
         //console.log(endList[index])
     })
-   
-    if (endList[0][0]==endList[endList.length-1][0]
+    //console.log(endList)
+    return endList
+}
+//функция создания линии
+function createLine(arr){
+    let endList=[]
+    arr.forEach((ev, index)=>{
+        endList[index]=[]
+        let elem=ev.split(/\s{1,}\r\n/)
+        //console.log(elem)
+        elem.forEach((item,num)=>{
+            if (item!=''){
+                endList[num]=createPoint([item])[0]
+            }
+
+            /* let elem=item.split(/\s{1,}/)
+            console.log(elem)
+            endList[index][0]= index
+            endList[index][1]= parseFloat(elem[0])
+            endList[index][3]= calcToDec(elem[1])
+            endList[index][2]= calcToDec(elem[2]) */
+
+            /* console.log(endList[index]) */
+        })
+        
+    })
+    //console.log(endList)
+    return endList
+}
+
+//функция создания полигона
+function createPoligon(arr){
+    let endList=createLine(arr)
+    if (endList[0][1]==endList[endList.length-1][1]
         &&
-        endList[0][1]==endList[endList.length-1][1]){
+        endList[0][2]==endList[endList.length-1][2]){
         console.log('JR')
     }else{
         endList.push(endList[0])
         //console.log (arr)
     }
     //console.log(endList)
-    return [endList.reverse()]
+    return endList
 }
 
 
@@ -71,27 +107,87 @@ for (let i=0;i<input_data.rows.length;i++){
         if (num!=8){
             odject_data_set[ev[0]]=input_data.values[num][i]
         }else{
-            //console.log(i, input_data.values[num][i])
             odject_data_set[ev[0]]=input_data.values[num][i]
-            
         }
-        //console.log(num,ev[0])
-        //odject_data_set[ev[0]]=input_data.values[num][i]
-
     })
     let selectColon=odject_data_set[input_data.cols[8][0]]
     if(selectColon === null ){
+        //выбраковка пустых ячеек
+        odject_data_set.type='null'
         output_data_tible.push(odject_data_set)
     }else{
         //console.log(regexpCoordSys.lastIndex)
         //switch()
 
         //тут нужно разбирать и сортировать
+
+        let result=selectColon.match(regexpTable)
+        //console.log(i,Array.isArray( result))
+        //console.log(i,result.length)
+        /*if (result.length!=selectColon.match(regexpType).length){
+
+            console.log(selectColon.match(regexpType))
+            console.log(selectColon.match(regexpTable))
+            //console.log(selectColon.match(regexpString))
+            //console.log(result)
+            //console.log(JSON.stringify( selectColon))
+            console.log((selectColon))
+        }
+         if (selectColon.match(regexpType).length>1){
+            //console.log(regexpTable.exec(selectColon))
+           
+            //console.log(i,result.length)
+            console.log(i,Array.isArray( result))
+            //console.log(result)
+        } */
         if ( selectColon.match(regexpType)===null){
-            output_data_point.push(odject_data_set)
+            odject_data_set.type='NaN'
+            //odject_data_set.type='collection'
+            odject_data_set.crs=selectColon.match(regexpCoordSys)
+            odject_data_set.geometyArray=[]
+            output_data_NaN.push(odject_data_set)
             
         }else{
-            console.log(i, selectColon.match(regexpType) )
+            if (selectColon.match(regexpType).length>1){
+                odject_data_set.type='collection'
+                odject_data_set.crs=selectColon.match(regexpCoordSys)
+                odject_data_set.geometyArray=[]
+                output_data_collection.push(odject_data_set)
+            }else{
+                switch(selectColon.match(regexpType)[0]){
+                    case 'Мультиточка':{
+                        odject_data_set.type='point'
+                        odject_data_set.crs=selectColon.match(regexpCoordSys)
+                        odject_data_set.geometyArray=[]
+                        output_data_point.push(odject_data_set)
+                        break
+                    };
+                    case 'Линия':{
+                        console.log(selectColon.match(regexpType))
+                        console.log(selectColon.match(regexpType)[0])
+                        odject_data_set.type='line'
+                        odject_data_set.crs=selectColon.match(regexpCoordSys)
+                        odject_data_set.geometyArray=[]
+                        output_data_line.push(odject_data_set)
+                        break
+                    };
+                    case 'Полигон':{
+                        odject_data_set.type='poligon'
+                        odject_data_set.geometyArray=[]
+                        odject_data_set.crs=selectColon.match(regexpCoordSys)
+                        output_data_poligon.push(odject_data_set)
+                        break
+                    };
+                    default:{
+                        odject_data_set.type='collection'
+                        odject_data_set.geometyArray=[]
+                        odject_data_set.crs=selectColon.match(regexpCoordSys)
+                        output_data_collection.push(odject_data_set)
+                    }
+                }
+                //console.log(selectColon.match(regexpType).length)
+            }
+            //console.log(i, selectColon.match(regexpType) )   
         }
         //regexpCoordSys.lastIndex=0
     }
@@ -103,8 +199,22 @@ let maxi=0
 output_data_tible.forEach((ev, index)=>(
     maxi=Math.max(index,maxi)
 ))
-console.log(output_data_tible.length)
-console.log(output_data_point.length)
+console.log(0, output_data_point.length)
+console.log(1, output_data_line.length)
+console.log(2, output_data_poligon.length)
+console.log(3, output_data_collection.length)
+console.log(4, output_data_NaN.length)
+console.log(5, output_data_tible.length)
+let test_odj=output_data_NaN[0]
+let test_list=test_odj['Географические координаты угловых точек участка недр, верхняя и нижняя границы участка недр']
+console.log(test_odj.uid)
+console.log(test_odj)
+console.log(test_list)
+console.log(test_list.match(regexpTable))
+//console.log(createPoint(test_list.match(regexpTable)))
+//console.log(createLine(test_list.match(regexpTable)))
+//console.log(createPoligon(test_list.match(regexpTable)))
+
 /* input_data.cols.forEach((ev,num)=>{
     output_data[index][ev[0]]
     console.log(ev[0])
