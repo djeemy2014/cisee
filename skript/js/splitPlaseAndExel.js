c24584f04b1b005844bfa681da30f18ef614d917
Dmitiy Demidyuk work <djeemy2014@gmail.com>
2023-09-12 14:15:27 +0300

    SplinePlase

---
+++ skript/js/splitPlaseAndExel.js
@@ -0,0 +1,14 @@
+import XLSX from 'xlsx';
+import fs from 'fs'
+import path from 'path'
+import gdal from 'gdal-async'
+
+class SplinePlase {
+    constructor(){
+        this.oldPlase=oldPlase;
+        this.newPlase=newPlase;
+    }
+
+}
+
+export default SplinePlase
